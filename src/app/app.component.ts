27e0b84fb743ae6e908834be09deb62dd4b3fba6
Takuma Kira <takuma@kirakiraworx.com>
2020-11-30 01:43:35 +0900

    build: place the background image

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, HostBinding, OnInit } from '@angular/core';
 import { tap } from 'rxjs/operators';
 
 @Component({
@@ -8,7 +8,8 @@ import { tap } from 'rxjs/operators';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  private bgImgData: object = {};
+  @HostBinding('style.backgroundImage') backgroundImage = '';
+  private bgImgData: any;
   constructor(
     private http: HttpClient,
   ) {}
@@ -19,12 +20,17 @@ export class AppComponent implements OnInit {
     const bgImgDataStr = localStorage.getItem('bgImgData');
     if (bgImgDataStr) {
       this.bgImgData = JSON.parse(bgImgDataStr);
+      this.setBgImg();
     } else {
       const unsplashApiUrl = `https://api.unsplash.com/photos/random?client_id=${process.env.UNSPLASH_ACCESS_KEY}`;
       this.http.get(unsplashApiUrl).pipe(
         tap(res => localStorage.setItem('bgImgData', JSON.stringify(res))),
         tap(res => this.bgImgData = res),
+        tap(() => this.setBgImg()),
       ).subscribe();
     }
+  }
+  private setBgImg(): void {
+    this.backgroundImage = `url("${this.bgImgData.urls.full}")`;
   }
 }
