79eb801cfb328cb9f6720a8157e35bd9c294801b
Takuma Kira <takuma@kirakiraworx.com>
2020-11-30 00:57:58 +0900

    build: make the test pass

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [
         AppComponent
+      ],
+      imports: [
+        HttpClientTestingModule,
       ],
     }).compileComponents();
   });
