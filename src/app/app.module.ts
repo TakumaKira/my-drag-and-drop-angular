9941c723fabaa1410bae1896e2a37beba80548ea
Takuma Kira <takuma@kirakiraworx.com>
2020-12-01 15:08:13 +0900

    build: setup error tracking

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, ErrorHandler } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import Bugsnag from '@bugsnag/js';
+import { BugsnagErrorHandler } from '@bugsnag/plugin-angular';
 
 import { AppComponent } from './app.component';
+
+// configure Bugsnag asap
+Bugsnag.start({ apiKey: '34323d0c8a8addefb93a5b24790d59b7' });
+// create a factory which will return the Bugsnag error handler
+export function errorHandlerFactory(): BugsnagErrorHandler {
+  return new BugsnagErrorHandler();
+}
 
 @NgModule({
   declarations: [
@@ -12,7 +21,11 @@ import { AppComponent } from './app.component';
     BrowserModule,
     HttpClientModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    { provide: ErrorHandler, useFactory: errorHandlerFactory },
+  ],
+  bootstrap: [
+    AppComponent,
+  ]
 })
 export class AppModule { }
