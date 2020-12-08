9511d60306aecff96e01d87e598dc634bb352b0d
Takuma Kira <takuma@kirakiraworx.com>
2020-12-08 17:31:01 +0900

    build: add ability to move lists

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component, HostBinding, OnInit } from '@angular/core';
+import { Component, ElementRef, HostBinding, HostListener, OnInit } from '@angular/core';
 import { UnsplashService } from './services/unsplash.service';
+import cloneDeep from 'lodash/cloneDeep';
 
 @Component({
   selector: 'app-root',
@@ -9,9 +10,19 @@ import { UnsplashService } from './services/unsplash.service';
 export class AppComponent implements OnInit {
   @HostBinding('style.backgroundImage') backgroundImage = '';
   bgImgData: any;
-  lists: any[] = [{ title: 'A', cards: ['aa', 'ab', 'ac'] }, { title: 'B', cards: ['ba', 'bb'] }, { title: 'C', cards: ['ca'] }];
+  lists: IList[] = [
+    { title: 'A', cards: ['aa'] },
+    { title: 'B', cards: ['ba'] },
+    { title: 'C', cards: ['ca'] },
+    { title: 'D', cards: ['da'] },
+    { title: 'E', cards: ['ea'] },
+  ];
+  listsBeforeMoved: IList[] | null = null;
+  dragging: number | null = null;
+  entering: number | null = null;
   constructor(
     public unsplash: UnsplashService,
+    private el: ElementRef<HTMLElement>,
   ) {}
   ngOnInit(): void {
     this.unsplash.getBgImgData().subscribe(bgImgData => {
@@ -51,4 +62,32 @@ export class AppComponent implements OnInit {
     }
     this.lists[i].cards[j] = textContent;
   }
+  onDragStartList(i: number): void {
+    this.dragging = i;
+    this.listsBeforeMoved = cloneDeep(this.lists);
+  }
+  onDragEnterList(i: number): void {
+    this.entering = i;
+    this.lists = cloneDeep(this.listsBeforeMoved) as IList[];
+    const moved = this.lists.splice((this.dragging as number), 1)[0];
+    this.lists.splice(i, 0, moved);
+  }
+  @HostListener('dragover', ['$event'])
+  onDragOverList(e: DragEvent): void {
+    e.preventDefault(); // Need to enable drop event and to disable "element back" animation
+  }
+  @HostListener('dragend')
+  onDragEndList(): void {
+    this.dragging = null;
+    this.entering = null;
+    this.listsBeforeMoved = null;
+  }
+  trackByItems(index: number, item: IList): number {
+    return index;
+  }
+}
+
+interface IList {
+  title: string;
+  cards: string[];
 }
