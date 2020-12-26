6b3c6ec85b55fc413b0b68b6caf4e5f9407f9dfd
Takuma Kira <takuma@kirakiraworx.com>
2020-12-27 01:58:47 +0900

    build: make test pass

--- src/app/services/unsplash.service.spec.ts
+++ src/app/services/unsplash.service.spec.ts
@@ -1,18 +1,25 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
 
+import { initialState } from '../store';
 import { UnsplashService } from './unsplash.service';
 
 describe('UnsplashService', () => {
   let service: UnsplashService;
+  let store: MockStore;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [
         HttpClientTestingModule,
       ],
+      providers: [
+        provideMockStore({ initialState }),
+      ]
     });
     service = TestBed.inject(UnsplashService);
+    store = TestBed.inject(MockStore);
   });
 
   it('should be created', () => {
--- src/app/store/bg-img.effects.spec.ts
+++ src/app/store/bg-img.effects.spec.ts
@@ -1,21 +1,29 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 import { provideMockActions } from '@ngrx/effects/testing';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
 import { Observable } from 'rxjs';
 
 import { BgImgEffects } from './bg-img.effects';
+import { initialState } from '../store';
 
 describe('BgImgEffects', () => {
   let actions$: Observable<any>;
+  let store: MockStore;
   let effects: BgImgEffects;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule,
+      ],
       providers: [
+        provideMockStore({ initialState }),
+        provideMockActions(() => actions$),
         BgImgEffects,
-        provideMockActions(() => actions$)
       ]
     });
-
+    store = TestBed.inject(MockStore);
     effects = TestBed.inject(BgImgEffects);
   });
 
