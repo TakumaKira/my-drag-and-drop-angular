fb9283eda6cfac9a7b44c5e4b58d6c219a93b1b0
Takuma Kira <takuma@kirakiraworx.com>
2020-12-27 00:35:14 +0900

    build: save state on local storage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import Bugsnag from '@bugsnag/js';
 import { BugsnagErrorHandler } from '@bugsnag/plugin-angular';
 import { EffectsModule } from '@ngrx/effects';
-import { StoreModule } from '@ngrx/store';
+import { StoreModule, USER_PROVIDED_META_REDUCERS } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { environment } from '../environments/environment';
@@ -13,6 +13,8 @@ import * as fromState from './store';
 import { CardEffects } from './store/card.effects';
 import { ListEffects } from './store/list.effects';
 import { BgImgDataEffects } from './store/bg-img-data.effects';
+import { getMetaReducers } from './store';
+import { LocalstorageService } from './services/localstorage.service';
 
 // configure Bugsnag asap
 Bugsnag.start({ apiKey: process.env.BUGSNAG_API_KEY as string });
@@ -29,11 +31,16 @@ export function errorHandlerFactory(): BugsnagErrorHandler {
     BrowserModule,
     HttpClientModule,
     StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
-    StoreModule.forRoot(fromState.reducers, { metaReducers: fromState.metaReducers }),
+    StoreModule.forRoot(fromState.reducers),
     EffectsModule.forRoot([ListEffects, CardEffects, BgImgDataEffects]),
   ],
   providers: [
     { provide: ErrorHandler, useFactory: errorHandlerFactory },
+    {
+      provide: USER_PROVIDED_META_REDUCERS,
+      deps: [LocalstorageService],
+      useFactory: getMetaReducers,
+    },
   ],
   bootstrap: [
     AppComponent,
--- src/app/store/index.ts
+++ src/app/store/index.ts
@@ -1,8 +1,11 @@
 import {
+  Action,
+  ActionReducer,
   ActionReducerMap,
   MetaReducer
 } from '@ngrx/store';
 import { environment } from '../../environments/environment';
+import { LocalstorageService } from '../services/localstorage.service';
 import { BgImgData, bgImgDataFeatureKey, bgImgDataInitialState, bgImgDataReducer } from './bg-img-data.reducer';
 import { Cards, cardsFeatureKey, cardsInitialState, cardsReducer } from './card.reducer';
 import { Lists, listsFeatureKey, listsInitialState, listsReducer } from './list.reducer';
@@ -29,5 +32,21 @@ export const reducers: ActionReducerMap<State> = {
   [orderFeatureKey]: orderReducer,
 };
 
-
 export const metaReducers: MetaReducer<State>[] = !environment.production ? [] : [];
+
+export const storeLocalstorage = (localstorageService: LocalstorageService): MetaReducer<State> => {
+  return (reducer: ActionReducer<State, Action>): ActionReducer<State, Action> => {
+    return (state, action) => {
+      if (action.type === '@ngrx/store/init') {
+        state = localstorageService.get();
+      }
+      const nextState = reducer(state, action);
+      localstorageService.set(nextState);
+      return nextState;
+    };
+  };
+};
+
+export const getMetaReducers = (localstorageService: LocalstorageService): MetaReducer<State>[] => {
+  return [...metaReducers, storeLocalstorage(localstorageService)];
+};
