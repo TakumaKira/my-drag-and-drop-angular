72c8614a946cc06195ca5363e5a91b45b7b8b969
Takuma Kira <takuma@kirakiraworx.com>
2020-12-29 18:37:17 +0900

    refactor: make test pass

--- src/app/components/card/card.component.spec.ts
+++ src/app/components/card/card.component.spec.ts
@@ -1,16 +1,23 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
 
+import { initialState } from '../../store/index';
 import { CardComponent } from './card.component';
 
 describe('CardComponent', () => {
   let component: CardComponent;
+  let store: MockStore;
   let fixture: ComponentFixture<CardComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CardComponent ]
+      declarations: [ CardComponent ],
+      providers: [
+        provideMockStore({ initialState }),
+      ],
     })
     .compileComponents();
+    store = TestBed.inject(MockStore);
   });
 
   beforeEach(() => {
